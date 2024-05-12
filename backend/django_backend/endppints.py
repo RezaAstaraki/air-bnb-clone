f2bd5440309fef350d692e5befeac36148d6beaf
Reza Astaraki <rezaastaraky@gmail.com>
2024-05-12 07:59:00 -0700

    fix api endpoints view

--- backend/django_backend/endppints.py
+++ backend/django_backend/endppints.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def endpoints_view(request):
     endpoints = {
         'hello':'dddd',
