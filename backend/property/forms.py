82b6c9f79c124fdfb704f65be32bd030454e4f1e
RezaAstarki <reza.astaraky@gmail.com>
2024-07-21 17:47:55 +0330

    fix submit form add home

--- backend/property/forms.py
+++ backend/property/forms.py
@@ -6,6 +6,6 @@ class AddPropertyForm(ModelForm):
 
     class Meta:
         model = Property
-        exclude = ['landlord']
+        exclude = ['landlord', 'favorite']
         # fields = '__all__'
         # fields = ['title', 'descriptions', 'price_per_night', 'bedrooms', 'bathrooms', 'guests', 'country', 'country_code', 'category', 'image']
